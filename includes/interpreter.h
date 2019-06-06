a7b16beba5fb4ea6d105524325f895141b7d4e31
Hyphi <nrechati@student.42.fr>
2019-06-06 17:48:54 +0200

    [INTERPRETER] Mapping

--- includes/interpreter.h
+++ includes/interpreter.h
@@ -6,11 +6,21 @@
 /*   By: nrechati <nrechati@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 12:51:14 by nrechati          #+#    #+#             */
-/*   Updated: 2019/06/06 12:51:55 by nrechati         ###   ########.fr       */
+/*   Updated: 2019/06/06 13:57:12 by nrechati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef INTERPRETER_H
 # define INTERPRETER_H
+
+/*
+*****************************************************
+********************** MAPING ***********************
+*****************************************************
+*/
+
+void	*group_to_job(void *data);
+void	*cmd_to_process(void *data);
+void	*action_to_redirects(void *data);
 
 #endif
