b1e7cbbc426ba848f6cf456f4574cf9b5e03a4a0
Cedric M'PASSI <cempassi@student.42.fr>
2019-06-13 17:33:31 +0200

    [ANALYZER] Valid command fix

--- srcs/module/analyzer/state/string_analyzer.c
+++ srcs/module/analyzer/state/string_analyzer.c
@@ -6,7 +6,7 @@
 /*   By: nrechati <nrechati@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/18 21:17:49 by ffoissey          #+#    #+#             */
-/*   Updated: 2019/06/13 01:49:21 by cempassi         ###   ########.fr       */
+/*   Updated: 2019/06/13 17:32:46 by cempassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,13 @@ void	flush_string(t_resolution *resolve)
 	action.type = A_ARGS;
 	while (ft_stcksize(&resolve->stack) != 0)
 	{
+		resolve->valid = 1;
 		token = ft_stckpop(&resolve->stack);
 		new_node = ft_lstnew(token, sizeof(t_token));
 		ft_lstadd(&action.data, new_node);
 	}
-	ft_stckpush(&resolve->tree_node, &action, sizeof(t_action));
-	resolve->valid = 1;
+	if (resolve->valid == 1)
+		ft_stckpush(&resolve->tree_node, &action, sizeof(t_action));
 	return ;
 }
 
