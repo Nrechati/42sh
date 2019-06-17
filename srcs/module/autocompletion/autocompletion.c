ecdf32d1397e84a99f0744cd12e7c35eb7cbfd12
Frederic FOISSEY <frederic.foissey@gmail.com>
2019-06-17 19:38:03 +0200

    [REFACTO]

--- srcs/module/autocompletion/autocompletion.c
+++ srcs/module/autocompletion/autocompletion.c
@@ -28,41 +28,44 @@ static char	*active_completion(char *input, char *completion)
 	return (NULL);
 }
 
-char	*autocompletion(char *input, t_registry *shell,
+static char	*send_rest(t_autocomplete *result, char *input)
+{
+	char	*completion;
+
+	completion = (char *)result->list->data;
+	completion += result->type == VARIABLE_BRACKET_TYPE ? 2 : 1;
+	return (active_completion(input, completion));
+}
+
+
+static void	reset_autocompletion(t_autocomplete *result)
+{
+	ft_lstdel(&result->list, NULL); // IS GOOD ?
+	ft_bzero(result, sizeof(t_autocomplete));
+}
+char		*autocompletion(char *input, t_registry *shell,
 								int col, uint64_t option)
 {
 	char						*completion;
 	static t_autocomplete		result;
-	static t_completion_fct		*get_completion[] = {
-												get_completion_cmd,
-												get_completion_var,
-												get_completion_var,
-												get_completion_file};
+	static t_completion_fct		*get_completion[] = {get_completion_cmd,
+									get_completion_var, get_completion_var,
+									get_completion_file};
 
-	(void)col;
 	completion = NULL;
 	if ((option & RESET_RESULT) || (option & NEW_SEARCH))
 	{
-		ft_lstdel(&result.list, NULL); // IS GOOD ?
-		ft_bzero(&result, (sizeof(result)));
+		reset_autocompletion(&result);
 		if (option & RESET_RESULT)
 			return (NULL);
 	}
-	if (option & NEW_SEARCH)
-	{
-		result.type = get_result_type(input, input == NULL ? 0 : ft_strlen(input));
-		completion = get_start_input(input, result.type) + 1;
-		//ft_printf("\ninput: '%s', modif_input: '%s', type: %d\n", input, completion, result.type);
-		get_completion[result.type](completion, &result, shell);
-		if (result.nb == 1)
-		{
-			input = completion;
-			completion = (char *)result.list->data;
-			completion += result.type == VARIABLE_BRACKET_TYPE ? 2 : 1;
-			return (active_completion(input, completion));
-		}
-		ft_mergesort(&result.list, lst_strcmp);
-		print_possibilities(&result, col);
-	}
+	result.type = get_result_type(input, input == NULL ? 0 : ft_strlen(input));
+	completion = get_start_input(input, result.type) + 1;
+	//ft_printf("\ninput: '%s', modif_input: '%s', type: %d\n", input, completion, result.type);
+	get_completion[result.type](completion, &result, shell);
+	if (result.nb == 1)
+		return (send_rest(&result, completion));
+	ft_mergesort(&result.list, lst_strcmp);
+	print_possibilities(&result, col);
 	return (NULL);
 }
