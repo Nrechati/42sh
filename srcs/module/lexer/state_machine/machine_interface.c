1a566e8d23abde1873c81cdf002f975f450a60b9
Frederic FOISSEY <frederic.foissey@gmail.com>
2019-06-20 02:32:11 +0200

    [DEV] expansion history

--- srcs/module/lexer/state_machine/machine_interface.c
+++ srcs/module/lexer/state_machine/machine_interface.c
@@ -35,8 +35,71 @@ void	space_lexer(t_lexer *machine)
 	}
 }
 
+static char *get_search(char *str)
+{
+	t_vector	*search;
+	char		*tmp;
+
+	search = vct_new(0);
+	while (ft_strchr(ALLCHAR, *str) == NULL && *str != ' ' && *str != '\t')
+		vct_add(search, *str++);
+	tmp = ft_strdup(search->buffer);
+	vct_del(&search);
+	return (tmp);
+}
+
+void	call_history(t_lexer *machine, size_t initial_scale, uint64_t option)
+{
+	char	*search;
+	char	*replace;
+	size_t	len;
+
+	search = NULL;
+	if (machine->input->buffer[machine->index + initial_scale - 1] == '!')
+		replace = history(NULL, NULL, option);
+	else
+	{
+		search = get_search(machine->input->buffer
+				+ machine->index + initial_scale);
+		if (ft_isnumeric(search) == TRUE)
+			replace = history(NULL, search, option | BY_ID);
+		else
+			replace = history(NULL, search, option | BY_NAME);
+	}
+	if (replace != NULL)
+	{
+		len = ft_strlen(search) + initial_scale;
+		vct_replace_string(machine->input, machine->index,
+				machine->index + len, replace);
+	}
+	else
+	{
+		ft_dprintf(2, "42sh: %s: event not found", search);
+		/// EXIT LEXER
+	}
+	history(NULL, NULL, RESET_HEAD);
+	ft_strdel(&replace);
+	ft_strdel(&search);
+}
+
+void	history_replace(t_lexer *machine)
+{
+	if (get_input(machine, CUR_CHAR) == '!')
+	{
+		ft_printf("'%c'\n", get_input(machine, NEXT_CHAR));
+		if (get_input(machine, NEXT_CHAR) == '!')
+			call_history(machine, 2, GET_ENTRY | PREV);
+		else if (ft_isalnum(get_input(machine, NEXT_CHAR)) == TRUE)
+			call_history(machine, 2, GET_ENTRY);
+		else if (get_input(machine, NEXT_CHAR) == '-'
+			&& ft_isalnum(get_input(machine, NEXT_NEXT_CHAR)) == TRUE)
+			call_history(machine, 3, GET_ENTRY | REL);
+	}
+}
+
 void	start_lexer(t_lexer *machine)
 {
+//	history_replace(machine);
 	if (get_input(machine, CUR_CHAR) == '\0')
 		machine->state = assign_special(machine) ? L_SPACE : L_END;
 	else if (get_input(machine, CUR_CHAR) == '\\')
