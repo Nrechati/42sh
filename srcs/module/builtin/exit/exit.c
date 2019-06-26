68b034bb7e9b3ba83100039a98346dd708182741
Sébastien Küppers <sebastien.kuppers@gmail.com>
2019-06-26 11:36:25 +0200

    [JobCTL] Exit ca degage

--- srcs/module/builtin/exit/exit.c
+++ srcs/module/builtin/exit/exit.c
@@ -6,7 +6,7 @@
 /*   By: nrechati <nrechati@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/07 11:59:35 by skuppers          #+#    #+#             */
-/*   Updated: 2019/06/25 16:07:30 by skuppers         ###   ########.fr       */
+/*   Updated: 2019/06/26 11:36:08 by skuppers         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,6 @@ int8_t			exit_blt(t_registry *shell, char **av)
 {
 	int		ret;
 
-	if (shell->job_list != NULL)
-	{
-		ft_printf("They are stopped jobs.\n");
-		return (FAILURE);
-	}
 	++av;
 	if (*av != NULL)
 	{
