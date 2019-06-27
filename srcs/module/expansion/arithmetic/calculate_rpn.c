f464d6c11504606f4b6903c2c8bfa250c6aa55db
Hyphi <nrechati@student.42.fr>
2019-06-27 14:45:04 +0200

    [MATHS] Unary OK and Norme v01

--- srcs/module/expansion/arithmetic/calculate_rpn.c
+++ srcs/module/expansion/arithmetic/calculate_rpn.c
@@ -6,27 +6,23 @@
 /*   By: nrechati <nrechati@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/18 11:57:30 by nrechati          #+#    #+#             */
-/*   Updated: 2019/06/27 10:43:39 by nrechati         ###   ########.fr       */
+/*   Updated: 2019/06/27 14:43:56 by nrechati         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sh21.h"
 
-static void		do_the_math(t_rpn_tk *first, t_rpn_tk *second, t_rpn_tk *curr)
+static void		do_logical(t_rpn_tk *first, t_rpn_tk *second, t_rpn_tk *curr)
 {
-	if (curr->value.type & DIVIDE || curr->value.type & MODULO)
-	{
-		if (second->value.digit == 0)
-		{
-			ft_printf("Division by zero");
-			return;
-		}
-	}
 	if (curr->value.type & (OPERATOR & AND))
 		first->value.digit = (first->value.digit && second->value.digit);
 	else if (curr->value.type & (OPERATOR & OR))
 		first->value.digit = (first->value.digit || second->value.digit);
-	else if (curr->value.type & (OPERATOR & LESS))
+}
+
+static void		do_compare(t_rpn_tk *first, t_rpn_tk *second, t_rpn_tk *curr)
+{
+	if (curr->value.type & (OPERATOR & LESS))
 		first->value.digit = (first->value.digit < second->value.digit);
 	else if (curr->value.type & (OPERATOR & LESSEQ))
 		first->value.digit = (first->value.digit <= second->value.digit);
@@ -40,47 +36,94 @@ static void		do_the_math(t_rpn_tk *first, t_rpn_tk *second, t_rpn_tk *curr)
 		first->value.digit = (first->value.digit != second->value.digit);
 	else if (curr->value.type & (OPERATOR & EQUAL))
 		first->value.digit = (first->value.digit == second->value.digit);
-	else if (curr->value.type & (OPERATOR & TIMES))
+}
+
+static void		do_high_op(t_rpn_tk *first, t_rpn_tk *second, t_rpn_tk *curr)
+{
+	if (curr->value.type & (OPERATOR & TIMES))
 		first->value.digit *= second->value.digit;
-	else if (curr->value.type & (OPERATOR & DIVIDE))
-		first->value.digit /= second->value.digit;
-	else if (curr->value.type & (OPERATOR & MODULO))
-		first->value.digit %= second->value.digit;
-	else if (curr->value.type & (OPERATOR & PLUS))
+	else
+	{
+		if (second->value.digit == 0)
+			ft_printf("Division by zero");
+		else if (curr->value.type & (OPERATOR & DIVIDE))
+			first->value.digit /= second->value.digit;
+		else if (curr->value.type & (OPERATOR & MODULO))
+			first->value.digit %= second->value.digit;
+	}
+}
+
+static void		do_low_op(t_rpn_tk *first, t_rpn_tk *second, t_rpn_tk *curr)
+{
+	if (curr->value.type & (OPERATOR & PLUS))
 		first->value.digit += second->value.digit;
 	else if (curr->value.type & (OPERATOR & MINUS))
 		first->value.digit -= second->value.digit;
+}
+
+static void		do_math(t_rpn_tk *first, t_rpn_tk *second, t_rpn_tk *curr)
+{
+	if (curr->value.type & (PRECEDENCE & AND))
+		do_logical(first, second, curr);
+	else if (curr->value.type & (PRECEDENCE & EQUAL))
+		do_compare(first, second, curr);
+	else if (curr->value.type & (PRECEDENCE & TIMES))
+		do_high_op(first, second, curr);
+	else if (curr->value.type & (PRECEDENCE & PLUS))
+		do_low_op(first, second, curr);
+}
+
+static void		do_unary(t_rpn_tk *number, t_rpn_tk *curr)
+{
+	if (curr->value.type & (OPERATOR & UNARY_MINUS))
+		number->value.digit = -1 * number->value.digit;
+	else if (curr->value.type & (OPERATOR & UNARY_PLUS))
+		number->value.digit = 1 * number->value.digit;
+}
+
+static int8_t 	handle_operator(t_rpn_tk *curr, t_stack *solve)
+{
+	t_rpn_tk		*first;
+	t_rpn_tk		*second;
+
+	first = NULL;
+	second = NULL;
+	if (ft_stcksize(solve) < 2)
+	{
+		if ((curr->value.type & OPERATOR) == UNARY_MINUS
+				|| (curr->value.type & OPERATOR) == UNARY_PLUS)
+		{
+			ft_dprintf(2, "Expression unsolvable\n");
+			return (FAILURE);
+		}
+		first = ft_stckpop(solve);
+		do_unary(first, curr);
+	}
+	else
+	{
+		second = ft_stckpop(solve);
+		first = ft_stckpop(solve);
+		do_math(first, second, curr);
+	}
+	ft_stckpush(solve, first, sizeof(t_rpn_tk));
+	//destroy second;
+	return (SUCCESS);
 }
 
 int64_t			calculate_rpn(t_stack *rpn)
 {
 	int64_t		result;
 	t_stack		solve;
-	t_rpn_tk	*second;
-	t_rpn_tk	*first;
 	t_rpn_tk	*curr;
 
 	ft_stckinit(&solve);
 	while (ft_stcksize(rpn) > 0)
 	{
-		first = NULL;
-		second = NULL;
 		curr = ft_stckpop(rpn);
 		if (curr->type == RPN_NUMBER)
 			ft_stckpush(&solve, curr, sizeof(t_rpn_tk));
 		else if (curr->type == RPN_OPERATOR)
-		{
-			if (ft_stcksize(&solve) < 2)
-			{
-				ft_dprintf(2, "Expression unsolvable\n");
-				return (FAILURE);
-			}
-			second = ft_stckpop(&solve);
-			first = ft_stckpop(&solve);
-			do_the_math(first, second, curr);
-			ft_stckpush(&solve, first, sizeof(t_rpn_tk));
-			//destroy second;
-		}
+			handle_operator(curr, &solve);
 		else
 			ft_dprintf(2, "Wrong token in RPN stack\n");
 	}
